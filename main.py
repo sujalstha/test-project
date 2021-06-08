9c9cb72a2e5a0713f6b17f8ccae1de2fa9b9e5a5
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-08 16:07:15 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+print("Hello Github")
+for i in range(2):
+    print("you there?")
+print("This is Sujal trying to enter the realm of Github")
+
+x = random.randint(0, 2)
+if x > 0:
+    print("You may enter Sujal to the Github realm")
+else:
+    print("Please leave from the Github porch")
